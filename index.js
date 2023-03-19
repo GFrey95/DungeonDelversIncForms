0053b93c259692f1f4020cd0f3d417ef23513ab3
Andy Frey <andyfrey95@gmail.com>
2023-03-19 02:39:44 -0600

    first attempt

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
 
-mongoose.connect(keys.mongoURI);
+mongoose
+  .connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to mongodb"))
+  .catch((err) => console.log("Could not connect to mongodb", err));
 
 const app = express();
 
-app.use(
-  cookieSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-require('./routes/authRoutes')(app);
-
 const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT);
